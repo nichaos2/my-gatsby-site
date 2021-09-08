5d1b0e836931f6169814b30ff2c6b8258745294f
nichaos2 <nick.ioannou86@gmail.com>
2021-09-08 16:21:02 +0200

    finish part 2 - links, elements, css

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from "../components/layout"
+
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+        <p>Hi there! I go by the name Nikos and I built this page with gatsby to show to Stathis.</p>
+      </Layout>
+
+)
+}
+
+export default AboutPage

8d50e18907c085779c1ef5f91b1a36cf4c23a2f7
nichaos2 <nick.ioannou86@gmail.com>
2021-09-10 16:34:55 +0200

    add blog posts, by using mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,14 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Nikolaos Ioannou",
   },
-  plugins: [ "gatsby-plugin-image",
-  "gatsby-plugin-sharp",
-  {
-    resolve: "gatsby-source-filesystem",
-    options: {
-      name: `blog`,
-      path: `${__dirname}/blog-posts`,
-    }
-  },],
+  plugins: ["gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog-posts`,
+      }
+    },
+    "gatsby-plugin-mdx",],
 };
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,36 +1,46 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
 
 
 const BlogPage = ({ data }) => {
   return (
-    
+
     <Layout pageTitle="My Blog Posts">
-    
+
       <p>My cool posts will go in here</p>
       <ul>
-      {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
-        ))
-      }
+        {
+          data.allMdx.nodes.map(node => (
+            <article key={node.name}>
+              <h2>{node.frontmatter.title}</h2>
+              <p>Posted: {node.frontmatter.date}</p>
+              <MDXRenderer>
+              {node.body}
+            </MDXRenderer>
+            </article>
+          ))
+        }
       </ul>
-    
+
     </Layout>
   )
 }
 
 export const query = graphql`
-  query {
-    allFile {
-      nodes {
-        name
+query {
+  allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+    nodes {
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY - HH:MM")
+        title
       }
+      id
+      body
     }
   }
+}
 `
 
 
